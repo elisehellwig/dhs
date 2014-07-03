25d8068e3e96279ddabd20b0c4870428204f2300
elisehellwig <echellwig@ucdavis.edu>
2014-07-03 13:47:09 -0700

    first draft of plot dhs data function

---
+++ functions/plot_functions.R
@@ -0,0 +1,61 @@
+library(sp)
+library(maptools)
+library(fields)
+library(scales)
+library(raster)
+library(classInt)
+
+plotDHS <- function(df, variable, region, colorpalette='PuBuGn', reverse=FALSE, point_size=0.5) {
+	#df <- is the data frame that contains the data that you want to plot
+	#df <- the variable that you want to plot, in quotes
+	#region <- the continent code for the continent you want to plot, or some other code for a region we have yet to come up with (ex Subsaharan Africa, or the Sahel) 
+	#(AF=Africa, AS=Asia, NO=North Amrica, SA=South America, OC=Oceana, EU=Europe
+	#datacols <- the columns that contain the data you want to plot (basically everything but lat/long)
+
+	#getting only the continent and variable of interest
+	crows <- which(df[,'contcode']==region)
+	dfc <- df[crows, ]
+	vars <- c('ISO3','contcode','year','v001','lon','lat','alt','URBAN_RURA',variable)
+	dfc <- dfc[,vars]
+	
+	cases <- complete.cases(dfc[,variable])
+	dfc <- dfc[cases,]
+
+
+	#creating spatial data frame
+	lonlat <- cbind(dfc[,'lon'],dfc[,'lat'])
+	spdf <- SpatialPointsDataFrame(coords=lonlat, data=dfc, proj4string=CRS("+proj=longlat +ellps=WGS84"))
+
+	#creating intervals for colors
+	breaks <- summary(spdf@data[,variable])[1:6]
+
+	#making sure intervals encompass the whole data set
+	if (breaks[6]<max(spdf@data[,variable],na.rm=TRUE)) {
+		breaks[6] <- breaks[6] + 0.1
+	}
+
+	if (breaks[1]>min(spdf@data[,variable], na.rm=TRUE)) {
+		breaks[1] <- breaks[1] - 0.1
+	}
+
+
+	#creating intervals for colors
+	cints <- classIntervals(spdf@data[,variable], style='fixed', fixedBreaks=breaks)
+
+	if (reverse==TRUE) {
+		colpal <- rev(brewer.pal(5, colorpalette))
+	} else {
+		colpal <- brewer.pal(5, colorpalette)
+	}
+	
+	colcode <- findColours(cints, colpal)
+
+
+	#plotting
+	data(wrld_simpl)
+	plot(spdf, pch=20, col=colcode, cex=point_size)
+	legend("bottomleft", title=paste(variable, 'in', region), legend=names(attr(colcode, "table")), fill=attr(colcode, "palette"), bty="n") ​
+	plot(wrld_simpl, add=TRUE)
+​
+
+}
