146d31b4a207c0d2e6617248e20c9dc64e8d5870
elisehellwig <echellwig@ucdavis.edu>
2014-07-03 15:56:51 -0700

    Added year and country functionality

--- functions/plot_functions.R
+++ functions/plot_functions.R
@@ -1,23 +1,55 @@
-library(sp)
-library(maptools)
-library(fields)
-library(scales)
-library(raster)
-library(classInt)
 
-plotDHS <- function(df, variable, region, colorpalette='PuBuGn', reverse=FALSE, point_size=0.5) {
+plotDHS <- function(df, variable, region, years='all', country='all', colorpalette='PuBuGn', reverse=FALSE, point_size=0.5) {
 	#df <- is the data frame that contains the data that you want to plot
 	#df <- the variable that you want to plot, in quotes
 	#region <- the continent code for the continent you want to plot, or some other code for a region we have yet to come up with (ex Subsaharan Africa, or the Sahel) 
 	#(AF=Africa, AS=Asia, NO=North Amrica, SA=South America, OC=Oceana, EU=Europe
-	#datacols <- the columns that contain the data you want to plot (basically everything but lat/long)
+	#country needs to be the ISO3 code for the country
 
-	#getting only the continent and variable of interest
-	crows <- which(df[,'contcode']==region)
-	dfc <- df[crows, ]
+
+	require(sp)
+	require(maptools)
+	require(fields)
+	require(scales)
+	require(raster)
+	require(classInt)
+
+	load('/Users/echellwig/Documents/Research/dhs/data/variablecodes.RData')
+	load('/Users/echellwig/Documents/Research/dhs/data/cttc.RData')
+
+
+
+	#getting only the region, country, year, and variable of interest
+	#region
+	reg.rows <- which(df[,'contcode']==region)
+	dfc <- df[reg.rows, ]
+	
+	#variable
 	vars <- c('ISO3','contcode','year','v001','lon','lat','alt','URBAN_RURA',variable)
 	dfc <- dfc[,vars]
-	
+
+	#country
+	if (country=='all') {
+		iso.rows <- 1:dim(dfc)[1]
+		reg.name <- region
+	} else {
+		reg.name <- country
+		iso.name <- ctt[ctt$countryname==country,'ISO3']
+		iso.rows <- which(dfc[,'ISO3']==iso.name)
+	}
+	dfc <- dfc[iso.rows,]
+
+	#year
+	if (years=='all') {
+		year.rows <- 1:dim(dfc)[1]
+	} else {
+		year.rows <- which(dfc[,'year']==years)
+		reg.name <- paste(reg.name, years)
+	}
+	dfc <- dfc[year.rows,]
+
+
+	#gets rid of NAs etc.
 	cases <- complete.cases(dfc[,variable])
 	dfc <- dfc[cases,]
 
@@ -51,11 +83,22 @@ plotDHS <- function(df, variable, region, colorpalette='PuBuGn', reverse=FALSE, 
 	colcode <- findColours(cints, colpal)
 
 
+	#gets variable name (not code)
+	vn <- varcodes[varcodes$varcode==variable,'varname']
+
 	#plotting
 	data(wrld_simpl)
 	plot(spdf, pch=20, col=colcode, cex=point_size)
-	legend("bottomleft", title=paste(variable, 'in', region), legend=names(attr(colcode, "table")), fill=attr(colcode, "palette"), bty="n") ​
+	legend("bottomleft", title=paste(vn, 'in', reg.name), legend=names(attr(colcode, "table")), fill=attr(colcode, "palette"), bty="n") ​
 	plot(wrld_simpl, add=TRUE)
 ​
 
 }
+
+
+
+
+
+
+
+
