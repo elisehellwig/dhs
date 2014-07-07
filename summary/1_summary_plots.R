7bf3e7e723a592b63cc133221a28bd686e9434a8
elisehellwig <echellwig@ucdavis.edu>
2014-07-07 09:48:45 -0700

    this commit loops over all the variables for plotting in 3 different continents (Africa, Asia and South America)

--- summary/1_summary_plots.R
+++ summary/1_summary_plots.R
@@ -1,39 +1,41 @@
 #this summarizes and plots aggregated DHS data
 setwd('/Users/echellwig/Drive/DHS/data/aggregated/')
-library(sp)
 library(maptools)
-library(fields)
-library(ggplot2)
-library(plyr)
-library(scales)
-library(raster)
-library(classInt)
 
 
 source('/Users/echellwig/Documents/Research/dhs/functions/plot_functions.R')
 
 #loads data
 data(wrld_simpl)
-load('KR_national.RData')
 load('KR_cluster_num.RData')
 load('KR_cluster_cat.RData')
 
 
-#separates out continents
-
+plotvars <- names(cln[11:length(cln)])
 ###########################Africa###########################################
 
-#average number of children under 5 per household
-plotDHS(cln, 'v137', 'AF')
+for (i in plotvars) {
+	ppath <- file.path('/Users','echellwig','Drive','DHS','projects','elise','summaryplots', paste0('africa', i, '.png'))
+	png(file=ppath)
+		plotDHS(cln, i, 'AF')
+	dev.off()
+}
 
-#height/age standard deviation
-plotDHS(cln, 'hw5', 'AF')
+###########################Asia###########################################
 
-#weight/height standard deviation
-plotDHS(cln, 'hw11', 'AF')
+for (i in plotvars) {
+	ppath <- file.path('/Users','echellwig','Drive','DHS','projects','elise','summaryplots', paste0('asia', i, '.png'))
+	png(file=ppath)
+		plotDHS(cln, i, 'AS', legendlocation='bottom')
+	dev.off()
+}
 
-plotDHS(cln, 'hw53', 'AF')
 
+###########################South America###########################################
 
-
-
+for (i in plotvars) {
+	ppath <- file.path('/Users','echellwig','Drive','DHS','projects','elise','summaryplots', paste0('sa', i, '.png'))
+	png(file=ppath)
+		plotDHS(cln, i, 'SA', legendlocation='bottomright')
+	dev.off()
+}
