f15a15656d221e125a361d9af506add27fc07015
elisehellwig <echellwig@ucdavis.edu>
2014-07-07 15:06:49 -0700

    This is a work in progress trying to figure out the best way to interpolate variables by country

--- summary/2_interpolation.R
+++ summary/2_interpolation.R
@@ -1,6 +1,29 @@
 setwd('/Users/echellwig/Drive/DHS/data/aggregated/')
 setwd("d:/gdrive/projects/DHS/")
 library(raster)
+library(fields)
 
+
+source('/Users/echellwig/Documents/Research/dhs/functions/general.R')
 
 load('KR_cluster_num.RData')
+
+cln1 <- cln[cln$countryname=='Nigeria',]
+
+ext1 <- extent(xmin=)
+
+
+
+lonlat1 <- cbind(cln1[,'lon'], cln1[,'lat'])
+
+fit1 <- Tps()
+
+
+
+
+spd <- DHSsp(cln1)
+
+spd <- spd
+
+fit <- Tps(lonlat1, cln1$hw5)
+
