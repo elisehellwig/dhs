f37b784b564cbd672645e71899ae66de3840aa98
elisehellwig <echellwig@ucdavis.edu>
2014-07-11 14:13:30 -0700

    added the rest of the categorical variables

--- summary/1_summary_plots.R
+++ summary/1_summary_plots.R
@@ -16,9 +16,9 @@ plotvarsn <- names(cln[11:length(cln)])
 plotvarsc <- c('h11', 'v113', 'v116', 'v119', 'v127', 'v149', 'v459')
 cats <- c('yes last two weeks', 'unprotected', 'no facility', 'yes', 'natural', 'incomplete primary','yes')
 
-#making the categorical variables we want to plot
+#making the categorical variables we want to plot, could do this using the reclassification but I don't feel like rerunning that code right now
 clc$h11[,4] <- sapply(1:length(clc$h11[,1]), function(i) sum(clc$h11[i,3], clc$h11[i,4]))
-clc$v116[,6] <- sapply(1:length(clc$v116[,1]), function(i) sum(clc$v116[i,1], clc$v116[i,6]))
+#clc$v116[,6] <- sapply(1:length(clc$v116[,1]), function(i) sum(clc$v116[i,1], clc$v116[i,6]))
 clc$v113[,12] <- sapply(1:length(clc$v113[,1]), function(i) sum(clc$v113[i,12], clc$v113[i,6]))
 attributes(clc$v113)$dimnames[[2]][12] <- 'unprotected'
 
